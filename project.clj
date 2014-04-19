8c78c24c6aabfdac41bb92f9a3d1d11dd995ac39
Philipp Küng <git@philippkueng.ch>
2014-04-19 13:48:32 +0200

    initial commit

---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject programmer-interests "0.1.0-SNAPSHOT"
+  :description "A set of functions to gather and visualize programmer interests"
+  :url "https://github.com/philippkueng/programmer-interests"
+  :license {:name "MIT"
+            :url "http://opensource.org/licenses/MIT"}
+  :dependencies [[org.clojure/clojure "1.5.1"]])
